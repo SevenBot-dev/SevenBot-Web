3908db8e9e6741dcff0ee0b08f4947f164bf1c7d
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-05-30 12:07:57 +0900

    Fix: 結果が反応しないバグを修正

--- captcha/static/js/login.js
+++ captcha/static/js/login.js
@@ -1,23 +1,19 @@
 target = document.getElementsByClassName('h-captcha')[0]
 
 async function checked(records) {
-    const response = await fetch("https://captcha.sevenbot.jp/check", {
+    response = await fetch("https://captcha.sevenbot.jp/check", {
         method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json',
         },
         redirect: 'follow',
-        referrerPolicy: 'no-referrer',
         body: JSON.stringify({
             token: document.getElementsByClassName('h-captcha')[0].childNodes[0].attributes.getNamedItem("data-hcaptcha-response").value,
             sessionid: (new URL(document.location)).searchParams.get("id")
         })
     })
     if (response.status == 200) {
-        if (response.json()["success"]) {
+        if ((await response.json())["success"]) {
             location.href = "https://captcha.sevenbot.jp/success"
         }
     }
