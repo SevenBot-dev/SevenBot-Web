0cfb0b7239caebcb2a2111f6cb9a587f8996783d
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-11-24 17:24:46 +0900

    Improve: 見た目を改善

--- general/static/js/loadless.js
+++ general/static/js/loadless.js
@@ -54,6 +54,18 @@ async function jumpURL(href) {
   console.debug("%cJumping to " + href, "color:#0067e0")
   // console.debug(pageCaches[href])
   if (!pageCaches[href]) {
+
+    mainBlocker = document.getElementById("main-blocker")
+    mainBlocker.style.display = "block"
+    mainBlocker.style.pointerEvents = "auto"
+    mainBlocker.animate({
+      opacity: [0, 0.4]
+    }, {
+      duration: 200,
+      easing: "ease-in-out",
+      fill: "forwards"
+    })
+
     console.debug("Fetch and cacheing")
     response = await fetch(href, {
       method: "GET"
@@ -63,7 +75,16 @@ async function jumpURL(href) {
 
     singleheadBlock = txt.match(/<!-- block singlehead -->([\s\S]*)<!-- endblock singlehead -->/)[1]
     pageCaches[href] = makeCacheObject(dom)
-
+    mainBlocker.style.pointerEvents = "none"
+    mainBlocker.animate({
+      opacity: [0.4, 0]
+    }, {
+      duration: 200,
+      easing: "ease-in-out",
+      fill: "forwards"
+    }).finished.then(() => {
+      mainBlocker.style.display = "none"
+    })
   } else {
     console.debug("Loading cache")
     title = pageCaches[href].title
@@ -74,13 +95,23 @@ async function jumpURL(href) {
   console.debug("Replacing")
   document.querySelector("head title").innerHTML = title
   document.querySelector("head").innerHTML = document.querySelector("head").innerHTML.replace(
-    /<!-- block head -->[\s\S]*<!-- endblock head -->/,
-    `<!-- block head -->${headBlock}<!-- endblock head -->`
+    /<!-- block head -->([\s\S]*)<!-- endblock head -->/,
+    `<!-- block head -->${headBlock}<!-- tmpblock -->$1<!-- endtmpblock --><!-- endblock head -->`
   )
-  document.querySelector("block[block-name='main']").innerHTML = body
   document.querySelector("head").innerHTML = document.querySelector("head").innerHTML.replace(
     /<!-- block singlehead -->[\s\S]*<!-- endblock singlehead -->/,
     `<!-- block singlehead -->${singleheadBlock}<!-- endblock singlehead -->`)
+  promisies = [];
+  [...document.querySelectorAll("head link[rel=stylesheet]")].forEach(e => {
+    if (!e.sheet) {
+      promisies.push(new Promise(resolve => {
+        e.addEventListener("load", resolve)
+      }))
+    }
+  })
+  await Promise.all(promisies)
+  document.querySelector("block[block-name='main']").innerHTML = body
+
   console.debug("Running script")
   runScript()
   history.pushState(null, title, href)
@@ -89,6 +120,13 @@ async function jumpURL(href) {
   if (menuOpener.getAttribute("open") === "true") {
     menuOpener.click()
   }
+  setTimeout(() => {
+    document.querySelector("head").innerHTML = document.querySelector("head").innerHTML.replace(
+      /<!-- tmpblock -->([\s\S]*)<!-- endtmpblock -->/,
+      ``
+    )
+  }, 0)
+
   console.debug("%cDone", "color:#3ba55c")
 }
 
