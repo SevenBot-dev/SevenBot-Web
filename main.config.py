802943bb07db46c2152f016f1f76f102c75aba0e
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-12-23 15:38:17 +0900

    Fix: configを修正

--- main.config.py
+++ main.config.py
@@ -1,6 +1,6 @@
 wsgi_app = "main:app"
 with open(".env", "r") as file:
-    env = file.read().splitlines()
+    raw_env = file.read().splitlines()
 
 bind = "localhost:8080"
 worker_class = "uvicorn.workers.UvicornWorker"
